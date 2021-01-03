eba919df3cc8cd5dc32811203077aec03490540e
Martin89PL <marcingladkowski@gmail.com>
2021-01-03 10:55:55 +0100

    crash course: welcome,python_zen, numbers

---
+++ python_crash_course/numbers.py
@@ -0,0 +1,6 @@
+print(10-2)
+print(4+4)
+print(4*2)
+print(80/10)
+number = 7
+print(f'My favorite number is {number}')
--- python_crash_course/personal_welcome.py
+++ python_crash_course/personal_welcome.py
@@ -1,4 +1,5 @@
 name = 'Marcin'
 
+# prepare greetings message
 msg = f"hi! {name}. Do you want to learn Python ?"
 print(msg)
