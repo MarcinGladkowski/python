0f6e1879f3b3587d7087353a8da4f4ff4e3da9a5
Martin89PL <marcingladkowski@gmail.com>
2021-01-04 22:23:30 +0100

    crash course: for exercises

---
+++ python_crash_course/animals.py
@@ -0,0 +1,7 @@
+wild_animals = ['lion', 'panther', 'tiger']
+
+for animal in wild_animals:
+    print(f'{animal} is very dangerous animal!')
+
+
+print(f'That african animals are very wild and dangerous!')
---
+++ python_crash_course/pizza.py
@@ -0,0 +1,8 @@
+pizzas = ['Neapolitan', 'Sicilian', 'Californian']
+
+
+for pizza in pizzas:
+    print(f'I really like {pizza}!')
+
+
+print(f'I love each kind of pizza!')
