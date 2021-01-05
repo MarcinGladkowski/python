8f4ca0fff97852583be220933cff990a160549da
Martin89PL <marcingladkowski@gmail.com>
2021-01-05 21:54:25 +0100

    crash course: for exercises _4, for loops, tuple

--- python_crash_course/4_for_and_numbers.py
+++ python_crash_course/4_for_and_numbers.py
@@ -38,4 +38,47 @@ for x in to_cube_first_10:
 to_cube_first_10_comprehension = [x**3 for x in range(11)]
 print(to_cube_first_10_comprehension)
 
+# 4.10
+print(f'Three first numbers are {to_cube_first_10_comprehension[:3]}')
+print(f'Three numbers in the middle are {to_cube_first_10_comprehension[3:6]}')
+print(f'Three numbers in the end are {to_cube_first_10_comprehension[-3:]}')
 
+# 4.11
+pizzas = ['Neapolitan', 'Sicilian', 'Californian']
+
+friend_pizzas = pizzas[:]
+
+pizzas.append('Hawaii')
+friend_pizzas.append('Margarita')
+
+print('My favourite pizzas are:')
+for pizza in pizzas:
+    print(pizza)
+
+
+print('My friend favourite pizzas are:')
+for pizza in friend_pizzas:
+    print(pizza)
+
+
+# 4.12
+my_foods = ['pizza', 'falafel', 'pie']
+print('My favourite food are:')
+for food in my_foods:
+    print(food)
+
+
+# 4.13 Tuple
+print('\n4.13\n')
+buffet = ('pizza', 'pie', 'falafel', 'breakfast', 'sapper')
+
+for dinner in buffet:
+    print(dinner)
+
+
+# buffet[0] = 'another food' Throws Exception
+
+buffet = ('super dinner', 'supper dinner 2')
+
+for dinner in buffet:
+    print(dinner)
