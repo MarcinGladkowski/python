1e287d957fb96210fdd5e7ba2ee49323d7026526
Martin89PL <marcingladkowski@gmail.com>
2021-01-05 22:09:59 +0100

    crash course: statements, or, and, in

---
+++ python_crash_course/5_if_statements.py
@@ -0,0 +1,21 @@
+# 5.1 5.2
+color = 'green'
+name = 'Marcin'
+
+print(color == 'green')
+print(name.lower() == 'marcin')
+print(len(name) == 6)
+print(name == 'Marcin')
+print(name == 'marcin')
+print(len(name) > 6)
+print(len(name) >= 6)
+print([] is True)
+print('test' is True)
+print('test ' == 'test')
+print('test' == 'test')
+print(name in ['Marcin', 'Tomek'])
+print('Test' in ['Marcin', 'Tomek'])
+print('Test' not in ['Marcin', 'Tomek'])
+print(20 > 10)
+print(20 <= 21)
+print(True or False)
