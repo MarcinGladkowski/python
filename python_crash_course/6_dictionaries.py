026d0eff7ee66ee7d2c467b28724d24547077262
Martin89PL <marcingladkowski@gmail.com>
2021-01-07 19:00:50 +0100

    crash course: 6.5, 6.6, 6.7

--- python_crash_course/6_dictionaries.py
+++ python_crash_course/6_dictionaries.py
@@ -52,7 +52,67 @@ for key, value in programming.items():
     print(f"key: {key}, and description: {value}")
 
 
+# 6.5
+print('\n6.5\n')
+rivers_in_countries = {
+    'Nil': 'Egypt',
+    'Tamas': 'London',
+    'Mississippi': 'USA'
+}
+
+for river, country in rivers_in_countries.items():
+    print(f'{river} river flows through {country}')
 
 
+for river in rivers_in_countries.keys():
+    print(river)
+
+for country in rivers_in_countries.values():
+    print(country)
+
+# 6.6
+print('\n6.6\n')
+
+persons_survey = {
+    'Marcin': 'PHP',
+    'Łukasz': 'Python',
+    'Json': 'JavaScript'
+}
+
+new_persons = ['Thomas']
+
+for new_person in new_persons:
+    if new_person not in persons_survey:
+        print(f'Maybe {new_person} you can take an action ?')
+
+    for person in persons_survey:
+        print(f'Thanks for your answer {person}')
 
 
+# 6.7
+print('\n6.7\n')
+people = {
+    'racing_driver': {
+        'first_name': 'Robert',
+        'last_name': 'Kubica',
+        'age': 36,
+        'city': 'Krakow',
+    },
+    'founder': {
+        'first_name': 'Elon',
+        'last_name': 'Musk',
+        'city': 'California',
+        'age': 47
+    },
+    'rally_driver': {
+        'first_name': 'Sebastian',
+        'last_name': 'Ogier',
+        'city': 'Paris',
+        'age': 38
+    }
+}
+
+for person, user_info in people.items():
+    print(f'Person data: {person}')
+    for key, value in user_info.items():
+        print(f'{key}: {value}')
