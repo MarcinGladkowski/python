e1cf715c30967bd3008ba12f61a9df244e4f674e
Martin89PL <marcingladkowski@gmail.com>
2021-01-10 22:11:27 +0100

    crash course: up to 8.11

--- python_crash_course/8_.py
+++ python_crash_course/8_.py
@@ -41,3 +41,88 @@ def describe_city(city, country='Poland'):
 describe_city('Warsaw')
 describe_city(city='Krakow')
 describe_city('New York')
+
+
+# 8.6
+def city_country(city, country):
+    print(f'{city}, {country}')
+
+
+city_country('Warsaw', 'Poland')
+city_country(country='Germany', city='Berlin')
+city_country('London', country='Great Britain')
+
+
+# 8.7
+def make_album(band, album_name, singles=None):
+    result = {
+        'band': band,
+        'album_name': album_name
+    }
+    if singles:
+        result['singles'] = singles
+
+    return result
+
+
+print(make_album('Agnieszka Chylińska', 'Modern Rocking'))
+print(make_album('Happysad', 'Mów mi dobrze'))
+print(make_album('Myslowitz', 'Miec czy byc'))
+print(make_album('Myslowitz', 'Zycie to surfing', 18))
+
+
+# 8.8
+while True:
+    band = input("Add band name: ")
+
+    if band == 'q':
+        break
+
+    album_name = input("Add album name: ")
+
+    if album_name == 'q':
+        break
+
+    make_album(band, album_name)
+
+
+print('\n8.9\n')
+
+
+# 8.9
+def show_messages(messages):
+    for message in messages:
+        print(f'{message}')
+
+
+messages_to_show = ['log', 'debug', 'error']
+
+show_messages(messages_to_show)
+
+sent_messages = []
+
+
+
+# 8.10
+print('\n8.10\n')
+
+
+def send_messages(messages):
+    print(messages)
+    while messages:
+        sent = messages.pop()
+        sent_messages.append(sent)
+       # print(f'{sent}')
+
+
+send_messages(messages_to_show)
+print(messages_to_show)
+print(sent_messages)
+
+
+# 8.11
+print('\n8.11\n')
+print(messages_to_show)
+show_messages(messages_to_show[:])
+print(messages_to_show)
+print(sent_messages)
