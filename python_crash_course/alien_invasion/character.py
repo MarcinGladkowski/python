ba7d43b32175f19ba271ed9c45e7b656413fbd72
Martin89PL <marcingladkowski@gmail.com>
2021-01-18 22:13:54 +0100

    crash course: moving space ship

---
+++ python_crash_course/alien_invasion/character.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Character:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('assets/images/astronaut.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
