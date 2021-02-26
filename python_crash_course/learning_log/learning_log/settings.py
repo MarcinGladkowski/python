213ea824ac5d569686a41334055d61e45b05b2e0
Martin89PL <marcingladkowski@gmail.com>
2021-02-26 23:12:37 +0100

    crash course: entry manipulation, start on registration form

--- python_crash_course/learning_log/learning_log/settings.py
+++ python_crash_course/learning_log/learning_log/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # own applications
     'learning_logs',
+    'users',
+    # django framework applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- python_crash_course/learning_log/learning_logs/views.py
+++ python_crash_course/learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
     """main page"""
@@ -18,3 +19,52 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add new topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return render(request, 'learning_logs/topic.html', topic_id=topic_id)
+
+    context = {'form': form, 'topic': topic}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit saved entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'learning_logs/topic.html', topic_id=topic.id)
+
+    context = {'form': form, 'topic': topic, 'entry': entry}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
