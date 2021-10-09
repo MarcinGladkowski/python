a04597c5978fb2b097b93d41b153bd316cb49db4
MarcinGladkowski <marcingladkowski@gmail.com>
2021-10-09 09:38:41 +0200

    exercises 58 - 60 from 100 challenge

---
+++ Python_100_plus/exercise_60.py
@@ -0,0 +1,25 @@
+"""
+Write a program which accepts a sequence of words separated by whitespace as input to print the words composed of digits only.
+
+Example: If the following words is given as input to the program:
+
+2 cats and 3 dogs.
+
+Then, the output of the program should be:
+
+['2', '3']
+
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Hints:
+
+Use re.findall() to find all substring using regex.
+"""
+import re
+
+
+def select_numbers(input: str):
+    return re.findall('\d+', input)
+
+
+print(select_numbers('2 cats and 3 dogs.'))
