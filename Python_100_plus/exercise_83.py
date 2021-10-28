72acf75f8ee1559ddbe71903baaa1d0bfa76db20
MarcinGladkowski <marcingladkowski@gmail.com>
2021-10-28 07:57:47 +0200

    exercises from 83 to 88

--- Python_100_plus/exercise_83.py
+++ Python_100_plus/exercise_83.py
@@ -3,4 +3,11 @@ Please write a program to print the running time of execution of "1+1" for 100 t
 
 Hints: Use timeit() function to measure the running time.
 """
+import timeit
 
+
+def sum_numbers():
+    return 1 + 1
+
+
+print(timeit.timeit(stmt=sum_numbers, number=10000))
