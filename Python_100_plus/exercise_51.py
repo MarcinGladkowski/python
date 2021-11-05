9bef65df7a7716856e662f19d480be434960d763
MarcinGladkowski <marcingladkowski@gmail.com>
2021-11-05 14:50:48 +0100

    exercises till 99

---
+++ Python_100_plus/exercise_51.py
@@ -0,0 +1,15 @@
+"""
+Define a class named American and its subclass NewYorker.
+
+Hints:
+
+Use class Subclass(ParentClass) to define a subclass.
+"""
+
+
+class American:
+    pass
+
+
+class NewYorker(American):
+    pass
---
+++ Python_100_plus/exercise_84.py
@@ -0,0 +1,15 @@
+"""
+Please write a program to shuffle and print the list [3,6,7,8].
+
+Hints: Use shuffle() function to shuffle a list.
+"""
+import random
+
+
+elements = [3, 6, 7, 8]
+
+random.shuffle(elements)
+
+print(elements)
+
+
