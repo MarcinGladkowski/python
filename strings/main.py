f89e9e27590ef4c572ae4c653977fcebbdaaab6e
MarcinGladkowski <marcingladkowski@gmail.com>
2022-10-13 07:35:03 +0200

    string basics

---
+++ strings/main.py
@@ -0,0 +1,36 @@
+print(type("hello world"))
+
+'''
+class str
+fundamental data type
+sequence data type - each character has a index!
+can use '' or ""
+' "" '
+" '' "
+''' '''
+""" """
+Can contain any Unicode character - contains Emoji
+'''
+string = "He said \"What time is it \""
+
+len(string) # including spaces
+
+'''
+PEP8 - style guide
+
+break line in strings using 
+ - backslash
+ - ''' '''
+ - """ """ 
+'''
+
+
+"""
+Concatenation: string1 + string2
+Indexing: my_string[3], index -1 is the last element
+Slice: creating substring - dessert = "apple pie" => dessert[0:5] => "apple"
+"""
+
+
+
+
