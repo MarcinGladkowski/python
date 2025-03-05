2eab87ff5d4de0bcdd89545f40707fdf9177baf6
MarcinGladkowski <marcingladkowski@gmail.com>
2025-03-05 09:40:17 +0100

    using __init__.py for modules

--- modules_and_packages/main.py
+++ modules_and_packages/main.py
@@ -1,5 +1,17 @@
 """
 We can use __all__ what is imported when * is using for it.
+
+__init__.py -> they are optional from Python 3.3
+* can be empty files
+
+my_package/
+    __init__.py
+    some_module.py
+will allow you to run:
+
+import my_package.some_module
+# or
+from my_package import some_module
 """
 
 import sys
